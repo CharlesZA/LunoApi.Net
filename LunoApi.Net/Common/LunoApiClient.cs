acdcea23bb72a7d727e6efd1a497548479448376
riv1 <riv1@users.noreply.github.com>
2017-08-04 19:57:15 +0200

    refactoring

--- LunoApi.Net/Common/LunoApiClient.cs
+++ LunoApi.Net/Common/LunoApiClient.cs
@@ -18,28 +18,15 @@ namespace LunoApi.Net.Common
 
         public async Task<T> GetPublicData<T>(string command, params object[] parameters)
         {
-            var relativeUrl = CreateUrlParameters(command, parameters);
-            HttpClientHandler handler = new HttpClientHandler();
-            var uri = _lunoConfig.PublicAPIUrl + relativeUrl;
-            using (var client = new HttpClient(handler))
-            {
-
-                client.BaseAddress = new Uri(uri);
-                var result = await client.GetAsync(uri);
-                var resultString = await result.Content.ReadAsStringAsync();
-                try
-                {
-                    var output = Util.JsonSerializer.DeserializeObject<T>(resultString);
-                    return output;
-                }
-                catch (Exception e)
-                {                    
-                    throw new LunoException(resultString);
-                }               
-            }
+            return await GetData<T>(command, false, parameters);            
         }
 
         public async Task<T> GetAuthData<T>(string command, params object[] parameters)
+        {
+            return await GetData<T>(command, true, parameters);            
+        }
+
+        private async Task<T> GetData<T>(string command, bool authenticate, params object[] parameters)
         {
             var relativeUrl = CreateUrlParameters(command, parameters);
             HttpClientHandler handler = new HttpClientHandler();
@@ -48,8 +35,11 @@ namespace LunoApi.Net.Common
             {
 
                 client.BaseAddress = new Uri(uri);
-                var byteArray = Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _lunoConfig.ID, _lunoConfig.Secret));
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                if (authenticate)
+                {
+                    var byteArray = Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _lunoConfig.ID, _lunoConfig.Secret));
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                }
                 var result = await client.GetAsync(uri);
                 var resultString = await result.Content.ReadAsStringAsync();
                 try
@@ -58,10 +48,11 @@ namespace LunoApi.Net.Common
                     return output;
                 }
                 catch (Exception e)
-                {                    
+                {
                     throw new LunoException(resultString);
                 }
             }
+
         }
 
         private static string CreateUrlParameters(string command, object[] parameters)
